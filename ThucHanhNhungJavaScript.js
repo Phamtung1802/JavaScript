ebbf5ebcc559b5fcf89117aecd01ca984ba88436
unknown <phamtung.0218@gmail.com>
2020-06-08 08:41:06 +0700

    Them Bai Thuc Hanh Nhung JavaScript

---
+++ ThucHanhNhungJavaScript.js
@@ -0,0 +1,13 @@
+function showMessageAlert(){
+    alert('Xin chào!');
+}
+function showMessageGetElement(){
+document.getElementById('messageDiv').innerHTML = 'Xin chào!';
+
+}
+function showMessageConsole(){
+console.log('Xin chào!');
+}
+function showMessageWWrite(){
+document.write('Xin chào!');
+}
