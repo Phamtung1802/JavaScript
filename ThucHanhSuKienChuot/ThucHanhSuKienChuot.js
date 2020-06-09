e364a2cc35e9d1e2408a0e6664bb7bec36654762
unknown <phamtung.0218@gmail.com>
2020-06-09 11:08:13 +0700

    add ThucHanhSuKienChuot

---
+++ ThucHanhSuKienChuot/ThucHanhSuKienChuot.js
@@ -0,0 +1,12 @@
+
+let imgObj = null;
+window.onload = function init() {
+    imgObj = document.getElementById('myImage');
+    console.log(imgObj);
+    imgObj.style.position = 'relative';
+    imgObj.style.left = '0px';
+}
+function moveRight() {
+    imgObj.style.left = parseInt(imgObj.style.left) + 10 + 'px';
+}
+
